b524e9b660b4b13a4255e32f7f28b1924c8f4448
tsengyushiang <tsengyushiang@gmail.com>
2019-03-13 23:30:00 +0800

    sprite animation done

---
+++ Assets/Scenes/script/animalMove.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animalMove : MonoBehaviour {
+
+    public float speed;             //Floating point variable to store the player's movement speed.
+
+    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+    float moveHorizontal = 0;
+    float moveVertical = 0;
+    
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        if (GetComponent<animationNormal>().RemainLockPlayTime != 0)
+        {
+            rb2d.velocity = Vector2.zero;
+            return;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            moveHorizontal = 1;
+            moveVertical = 0;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow)) {
+            moveHorizontal = -1;
+            moveVertical = 0;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            moveHorizontal = 0;
+            moveVertical = 1;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            moveHorizontal = 0;
+            moveVertical = -1;
+        }
+        else
+        {
+            if (!Input.anyKey)
+            {
+                moveHorizontal = 0;
+                moveVertical = 0;
+            }
+        }        
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+     
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        rb2d.velocity=movement * speed;
+
+    }
+}
---
+++ Assets/Scenes/script/animationNormal.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class animationNormal : MonoBehaviour {
+
+    private Material material;
+
+    [System.Serializable]
+    public struct animates
+    {
+        public string Name;
+        public KeyCode key;
+        public bool lockwhenPlay;
+        public int lockPlayTimes;
+        public Texture2D[] normalMaps;
+        public Sprite[] Normalsprites;
+        public Sprite[] Bleedingsprites;
+    };
+
+    public animates[] Animations;
+
+    // kind of animation
+    private int currentState=0;
+    // which sprite
+    private float currentMotion=0;
+    public float speed=0.05f;
+
+    private bool Isbleeding = false;
+    public int RemainLockPlayTime = 0;
+    private bool AnyBtnDown = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        material.EnableKeyword("_NORMALMAP");
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void Update()
+    {       
+        if (RemainLockPlayTime == 0) {
+            for (int i = 0; i < Animations.Length; i++)
+            {
+                AnyBtnDown = true;
+                if (Input.GetKey(Animations[i].key))
+                {
+                    if (i != currentState)
+                        currentMotion = 0.0f;
+
+                    currentState = i;
+
+                    if (Animations[i].lockwhenPlay == true)
+                    {
+                        RemainLockPlayTime = Animations[i].lockPlayTimes;
+                    }
+                    break;
+                }
+                AnyBtnDown = false;
+            }
+        }
+        
+
+        if (AnyBtnDown == true)
+        {
+
+            if(Isbleeding==true)
+                GetComponent<SpriteRenderer>().sprite = Animations[currentState].Bleedingsprites[(int)currentMotion];
+            else
+                GetComponent<SpriteRenderer>().sprite = Animations[currentState].Normalsprites[(int)currentMotion];
+
+            material.SetTexture("_BumpMap", Animations[currentState].normalMaps[(int)currentMotion]);
+
+            Destroy(GetComponent<PolygonCollider2D>());
+            gameObject.AddComponent<PolygonCollider2D>();
+
+            currentMotion += speed;
+            if ((int)currentMotion >= Animations[currentState].Normalsprites.Length)
+            {
+                currentMotion = 0;
+                RemainLockPlayTime--;
+                if (RemainLockPlayTime <= 0)
+                    RemainLockPlayTime = 0;
+            }
+        }
+    }
+
+}
